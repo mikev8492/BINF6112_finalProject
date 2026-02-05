a9ca18b343e60f69bdb4534f665281949d05624a
kwill423 <kwill423@charlotte.edu>
2026-02-05 14:32:55 -0500

    Update Pseudocode.py

--- Pseudocode.py
+++ Pseudocode.py
@@ -1,3 +1,5 @@
 '''
 The following file describes the objectives of each function and the corresponding steps to achieve the correct output.
+The link below is a known plasmid that we can use for testing our code:
+https://www.ncbi.nlm.nih.gov/nuccore/U13852.1 
 '''
