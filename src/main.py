ee308d013e87b2336a43aa8e4791373c50b85866
Michael Villarreal <mikev8492@gmail.com>
2026-02-05 14:55:49 -0500

    Document main function with setup and user input details
    Added detailed comments outlining the main function's responsibilities and planned features.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,18 @@
 """
 This file contains the main function that will perform the logic for the program. 
+
+1. Setup:
+  - function to pull list of enzymes from online resource and store formatted csv table in database folder
+
+2. User Input:
+  - function to handle how a user would input a sequence and select enzymes to cut with.
+
+3. Parser:
+  - function to parse user input sequence using each restriction enzyme
+  - Need to store the results (cut sites, name of enzyme, etc.)
+
+4. Results:
+  - function to render results:
+    - plasmid map with cut sites and name of enzymes identified 
+    - option to restart or edit the map?
 """
